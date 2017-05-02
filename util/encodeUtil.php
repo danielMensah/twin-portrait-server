ceed7bd7d3c4c51b9e4886c295d4cd745213a6cf
Unknown <k1439189@kingston.ac.uk>
2017-05-02 19:00:34 +0100

    New JSON encode

--- util/encodeUtil.php
+++ util/encodeUtil.php
@@ -5,13 +5,12 @@ function encodeUserToken($u_auth, $p_auth, $u_token){
         $u_token = 0;
     }
     $jarr = array(
-        array("label" => "u_auth", "data" => $u_auth),
-        array("label" => "p_auth", "data" => $p_auth),
-        array("label" => "u_token", "data" => $u_token)
-    );
-    //$jarr = array();
+        'u_auth' => $u_auth,
+        'p_auth' => $p_auth,
+        'u_token' => $u_token
+        );
     /*$jarr[u_auth] = $u_auth;
     $jarr[p_auth] = $p_auth;
     $jarr[u_token] = $u_token;*/
-    return json_encode($jarr);
+    return json_encode($jarr, JSON_NUMERIC_CHECK);
 }

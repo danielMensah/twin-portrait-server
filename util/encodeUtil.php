33f0e9da16588b87d744cfadf54233559df3e9e9
Unknown <k1439189@kingston.ac.uk>
2017-05-02 21:12:32 +0100

    Testing JSON object change
    Testing new constructed JSON
    
    
    Fixed syntax
    
    
    Corrected Syntax
    
    
    Corrected Syntax 2
    
    
    New JSON encode

--- util/encodeUtil.php
+++ util/encodeUtil.php
@@ -4,10 +4,13 @@ function encodeUserToken($u_auth, $p_auth, $u_token){
         $p_auth = 0;
         $u_token = 0;
     }
-    $jarr = array();
-    $jarr['u_auth'] = $u_auth;
-    $jarr['p_auth'] = $p_auth;
-    $jarr['u_token'] = $u_token;
-    //array_push($jarr, $u_auth, $p_auth, $u_token);
-    return json_encode($jarr);
+    $jarr = array(
+        'u_auth' => $u_auth,
+        'p_auth' => $p_auth,
+        'u_token' => $u_token
+        );
+    /*$jarr[u_auth] = $u_auth;
+    $jarr[p_auth] = $p_auth;
+    $jarr[u_token] = $u_token;*/
+    return json_encode($jarr, JSON_NUMERIC_CHECK);
 }

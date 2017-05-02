4699c788479ecd6d39a1ac6ef31ad9c14ce575e1
Unknown <k1439189@kingston.ac.uk>
2017-05-02 18:55:24 +0100

    Testing new constructed JSON

--- util/encodeUtil.php
+++ util/encodeUtil.php
@@ -4,9 +4,14 @@ function encodeUserToken($u_auth, $p_auth, $u_token){
         $p_auth = 0;
         $u_token = 0;
     }
-    $jarr = array();
-    $jarr[u_auth] = $u_auth;
+    $jarr = array(
+        array("label" => "u_auth", "data" => $u_auth;
+        array("label" => "p_auth", "data" => $p_auth;
+        array("label" => "u_token", "data" => $u_token
+    );
+    //$jarr = array();
+    /*$jarr[u_auth] = $u_auth;
     $jarr[p_auth] = $p_auth;
-    $jarr[u_token] = $u_token;
+    $jarr[u_token] = $u_token;*/
     return json_encode($jarr);
 }

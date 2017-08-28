6e732539049c619481965dc3f8e5491e01ecda4d
danielMensah <mensahezros@gmail.com>
2017-08-28 15:28:29 +0100

    can now update

--- config/connection.php
+++ config/connection.php
@@ -18,18 +18,6 @@ function getConnection() {
         echo "Error!: " . $e->getMessage() . "<br/>";
         die();
     }
-
-//    $host_name = '127.0.0.1';
-//    $database = 'test';
-//    $user_name = 'root';
-//    $password = '';
-//    $connect = mysqli_connect($host_name, $user_name, $password, $database);
-//
-//    if (mysqli_connect_errno()) {
-//        die('<p>Failed to connect to MySQL: '.mysqli_connect_error().'</p>');
-//    } else {
-//        echo '<p>Connection to MySQL server successfully established.</p >';
-//    }
 }
 
 
--- model/imageModel.php
+++ model/imageModel.php
@@ -30,6 +30,55 @@ require '../util/urlGeneratorUtil.php';
         return $image_url;
     }
 
+    function updatePortrait($arrayOfLandmarks, $portraitUrl) {
+        $dbh = getConnection();
+
+        $face = $arrayOfLandmarks['face']['landmarkKey'];
+        $eye = $arrayOfLandmarks['eye']['landmarkKey'];
+        $eyebrows = $arrayOfLandmarks['eyebrows']['landmarkKey'];
+        $nose = $arrayOfLandmarks['nose']['landmarkKey'];
+        $lips = $arrayOfLandmarks['lips']['landmarkKey'];
+
+        $updatedLandmarks = getPortraitForUpdate($arrayOfLandmarks);
+
+        $sql = $dbh->prepare("UPDATE portrait SET $face = :face_value, $eye=:eye_value, $eyebrows=:eyebrows_value, $nose=:nose_value, $lips=:lips_value WHERE image_url=:portrait_url");
+        $sql->bindParam(':face_value', $updatedLandmarks['face'], PDO::PARAM_INT);
+        $sql->bindParam(':eye_value', $updatedLandmarks['eye'], PDO::PARAM_INT);
+        $sql->bindParam(':eyebrows_value', $updatedLandmarks['eyebrows'], PDO::PARAM_INT);
+        $sql->bindParam(':nose_value', $updatedLandmarks['nose'], PDO::PARAM_INT);
+        $sql->bindParam(':lips_value', $updatedLandmarks['lips'], PDO::PARAM_INT);
+        $sql->bindParam(':portrait_url', $portraitUrl, PDO::PARAM_STR);
+
+        $sql->execute();
+        echo "Updated";
+    }
+
+    function getPortraitForUpdate($arrayOfLandmarks) {
+        $dbh = getConnection();
+        $image_url = 'https://s3.eu-west-2.amazonaws.com/twinportraint/Portraits/_4uBmkGE2UUx5G-Moro9AYRaDGhgVXB8R-yy97AZgjzPfwNUVMA_0fe6GWe0=s512.jpg';
+
+        $sql = $dbh->prepare("SELECT * FROM portrait WHERE image_url = :image_url");
+        $sql->bindParam(':image_url', $image_url, PDO::PARAM_STR);
+        $sql->execute();
+        $sql->bindColumn($arrayOfLandmarks['face']['landmarkKey'], $face, PDO::PARAM_INT);
+        $sql->bindColumn($arrayOfLandmarks['eye']['landmarkKey'], $eye, PDO::PARAM_INT);
+        $sql->bindColumn($arrayOfLandmarks['eyebrows']['landmarkKey'], $eyebrows, PDO::PARAM_INT);
+        $sql->bindColumn($arrayOfLandmarks['nose']['landmarkKey'], $nose, PDO::PARAM_INT);
+        $sql->bindColumn($arrayOfLandmarks['lips']['landmarkKey'], $lips, PDO::PARAM_INT);
+        $sql->fetch(PDO::FETCH_BOUND);
+
+        $fetchedLandmarksValues = array(
+            'face' => $face+1,
+            'eye' => $eye+1,
+            'eyebrows' => $eyebrows+1,
+            'nose' => $nose+1,
+            'lips' => $lips+1
+        );
+
+        return $fetchedLandmarksValues;
+
+    }
+
 //    function moveUploadedFile($directory, UploadedFile $uploadedFile) {
 //        $extension = pathinfo($uploadedFile->getClientFilename(), PATHINFO_EXTENSION);
 //        $basename = bin2hex(random_bytes(8)); // see http://php.net/manual/en/function.random-bytes.php

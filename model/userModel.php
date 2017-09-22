97de4cdb42c9ff535077fb726a497297e534d29c
danielMensah <mensahezros@gmail.com>
2017-09-22 22:04:08 +0100

    feedback added

--- model/userModel.php
+++ model/userModel.php
@@ -5,23 +5,21 @@
         $user_name = $user['name'];
         $user_lastName = $user['lastName'];
         $email = strtolower($user['email']);
+        $feedback = $user['feedback'];
 
         if (!checkIfUserExists($email)) {
-            $sql = $dbh->prepare("INSERT INTO users ( name, lastName, email ) VALUES ( '$user_name', '$user_lastName', '$email')");
+            $sql = $dbh->prepare("INSERT INTO users ( name, lastName, email, feedback ) VALUES ( :user_name, :last_name, :email, :feedback )");
+            $sql->bindParam(':user_name', $user_name, PDO::PARAM_STR);
+            $sql->bindParam(':last_name', $user_lastName, PDO::PARAM_STR);
+            $sql->bindParam(':email', $email, PDO::PARAM_STR);
+            $sql->bindParam(':feedback', $feedback, PDO::PARAM_STR);
 
-            $response = [];
-            if ( $sql->execute()) {
-                $response = array(
-                    'response' => 'updated',
-                    'promoCode' => addPromoCode($email)
-                );
-            } else {
-                $response = array(
-                    'response' => 'error'
-                );
-            }
-
-            return json_encode($response);
+            return $sql->execute() ? json_encode(array(
+                'response' => 'updated',
+                'promoCode' => addPromoCode($email)
+            )) : json_encode(array(
+                'response' => 'error'
+            ));
         } else {
             return json_encode(array('response' => 'Email already exists'));
         }
@@ -34,7 +32,10 @@
         $dbh = getConnection();
         $promo_code = generatePromoCode();
 
-        $sql = $dbh->prepare("INSERT INTO promo_codes ( user_id, promo_code ) VALUES ((select id from users where email = '$email'), '$promo_code')");
+        $sql = $dbh->prepare("INSERT INTO promo_codes ( user_id, promo_code ) VALUES ((select id from users where email = :email), :promo_code)");
+        $sql->bindParam(':email', $email, PDO::PARAM_STR);
+        $sql->bindParam(':promo_code', $promo_code, PDO::PARAM_STR);
+
         $sql->execute();
 
         return $promo_code;
